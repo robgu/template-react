5b0d5f45ba8b62be3980d51e36d8ae37b97697bd
Rob.Gu <rob.gu@augmentum.com>
2018-01-11 16:42:59 +0800

    i18n: use this.i18n instead of this.props.i18n

--- src/plugins/ReduxEnhance.js
+++ src/plugins/ReduxEnhance.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
+import { Component } from 'react';
 import * as redux from 'react-redux';
 import { routerActions } from 'react-router-redux';
 import * as pagan from 'redux-pagan';
 
 let store = null;
 let translations = null;
-const cachedI18n = {};
 
 const format = (keys, ...args) => {
   let result = (keys || '').toString();
@@ -31,39 +31,40 @@ export const loadLang = (lang) => {
   }));
 };
 
-
-export const getLang = () => {
-  const i18nState = store.getState().i18n;
-  if (cachedI18n[i18nState.locale]) {
-    return cachedI18n[i18nState.locale];
+Component.prototype.$i18n = function (path, ...args) {
+  const { i18n: { data, locale } } = store.getState();
+  const keys = [locale, 'app'];
+  if (this.i18nRootPath) {
+    keys.push(this.i18nRootPath);
   }
 
-  // 第一个 key 一定是 app，写在这里后其它地方可以省略，同时起到缓存作用
-  const originalI18n = pagan.getLang(i18nState, 'app');
-  cachedI18n[i18nState.locale] = (...args) => {
-    const lastArg = args[args.length - 1];
-    if (_.isArray(lastArg)) {
-      args.pop();
-      return format(originalI18n(...args), ...lastArg);
-    } else if (_.isNumber(lastArg)) {
-      args.pop();
-      if (lastArg === 1) {
-        args.push('singular');
-      } else {
-        args.push('plural');
+  keys.push(path);
+  const key = keys.join('.');
+
+  const value = _.get(data, key);
+  if (typeof value === 'string') {
+    return format(value, ...args);
+  } else if (_.isObject(value)) {
+    if (typeof args[0] === 'number') {
+      if (args[0] === 1) {
+        return value.singular;
       }
 
-      return originalI18n(...args).s;
+      return value.plural;
     }
 
-    return originalI18n(...args).s;
-  };
+    return value;
+  }
 
-  return cachedI18n[i18nState.locale];
+  return value;
 };
 
-export const i18n = (...args) => {
-  return getLang()(...args);
+Component.prototype.$push = (...keys) => {
+  return store.dispatch(routerActions.push(...keys));
+};
+
+Component.prototype.$replace = (...keys) => {
+  return store.dispatch(routerActions.replace(...keys));
 };
 
 export const connect = (...args) => {
@@ -71,19 +72,13 @@ export const connect = (...args) => {
 
   args.unshift((...params) => {
     const props = (getProps ? getProps(...params) : {}) || {};
-    props.i18n = getLang();
     props.dispatch = store.dispatch;
-    props.push = (...keys) => {
-      return store.dispatch(routerActions.push(...keys));
-    };
-    props.replace = (...keys) => {
-      return store.dispatch(routerActions.replace(...keys));
-    };
-
     return props;
   });
 
   return redux.connect(...args);
 };
+
+export const i18n = Component.prototype.$i18n;
 
 export default connect;
